a1bc500c350f0b3d5e7e23ef0e7d64f314b362db
jesperlunpet <s140456@student.dtu.dk>
2020-01-08 11:01:04 +0100

    updateSize function to update the size of the tree and remember the last x position

--- PostScriptGenerator.fs
+++ PostScriptGenerator.fs
@@ -3,6 +3,16 @@ module DrawingTree.PostScriptGenerator
 open Tree
 open System.IO
 
+let rec updateSize tree size prevx =
+    let rec updateSizeChildren children size prevx =
+        match children with
+        | [] -> []
+        | child::children -> updateSize child size prevx::updateSizeChildren children size prevx
+    match tree with
+    | Node((a,x),children) -> Node((a,(prevx+x)*size),updateSizeChildren children size (x+prevx))
+
+// Synthesizing to PostScript
+
 let initText = "%!\n<</PageSize[1400 1000]/ImagingBBox null>> setpagedevice\n1 1 scale\n700 999 translate\nnewpath\n/Times-Roman findfont 10 scalefont setfont\n"
 
 let writeLabel label =  " (" + string label + ")" + " dup stringwidth pop 2 div neg 0 rmoveto show\n"
@@ -16,7 +26,7 @@ let writeLabel label =  " (" + string label + ")" + " dup stringwidth pop 2 div 
 
 let getSize child =
     match child with
-    | Node(((_,_),x),_) -> x
+    | Node((_,x),_) -> x
 
 // let rec adjustWidthToMiddle line =
 //     match line with
@@ -28,28 +38,30 @@ let lineto x y = string x + " " + string y + " lineto\n"
 
 let drawLine xs ys xe ye = (moveto xs ys) + (lineto xe ye)
 
-let drawMiddleLine children y xsize = 
-    let rec drawMiddleLine' children y xsize = 
+let drawMiddleLine children y = 
+    let rec drawMiddleLine' children y = 
         match children with
         | [] -> "" // No children: No line
-        | Node(((_,_),x),_)::[] -> lineto (x*xsize) y // Last element: Draw line to
-        | Node(((_,_),x),_)::children -> drawMiddleLine' children y xsize // The list is not empty: Go to next child
+        | Node((_,x),_)::[] -> lineto x y // Last element: Draw line to
+        | Node((_,x),_)::children -> drawMiddleLine' children y // The list is not empty: Go to next child
     match children with
     | [] -> ""
-    | Node(((_,_),x),_)::children -> moveto (x*xsize) y + drawMiddleLine' children y xsize
+    | Node((_,x),_)::[] -> "" // If it is the only element - no need to draw a line
+    | Node((_,x),_)::children -> moveto x y + drawMiddleLine' children y
     
-let drawFirstLine x y xsize children =
+let drawFirstLine x y children =
     match children with
     | [] -> "" // No children: No line
-    | _ -> drawLine (x*xsize) (y-10.0) (x*xsize) (y-40.0)
+    | _ -> drawLine x (y-10.0) x (y-40.0)
 
 
-let rec drawLines tree y xsize =
+let rec drawLines tree y =
     match tree with
-    | Node(((label:string,_),x:float),children) -> (moveto (x*xsize) y) + (writeLabel label) + (drawFirstLine x y xsize children) + (drawMiddleLine children (y-40.0) xsize) + (List.fold (fun acc elem -> acc+drawLine ((getSize elem)*xsize) (y-40.0) ((getSize elem)*xsize) (y-80.0)) "" children) + "stroke\n" + List.fold (fun acc elem -> acc+drawLines elem (y-90.0) xsize) "" children
+    | Node((label,x:float),children) -> (moveto x y) + (writeLabel (string label)) + (drawFirstLine x y children) + (drawMiddleLine children (y-40.0)) + (List.fold (fun acc elem -> acc+drawLine (getSize elem) (y-40.0) (getSize elem) (y-80.0)) "" children) + "stroke\n" + List.fold (fun acc elem -> acc+drawLines elem (y-90.0)) "" children
 
-let synth tree xsize = File.WriteAllText("file.ps", (initText + (drawLines tree -10.0 xsize) + "showpage"))
+let generateTree tree = File.WriteAllText("file1.ps", (initText + (drawLines tree -10.0) + "showpage"))
 
-let testTree = Node(("P",0.0),[Node(("A",0.0),[Node(("A1",0.0),[]);Node(("A2",0.0),[]);Node(("A3",0.0),[])]);Node(("B",0.0),[Node(("B1",0.0),[])]);Node(("C",0.0),[]);Node(("D",0.0),[]);Node(("E",0.0),[]);Node(("F",0.0),[]);Node(("G",0.0),[])])
-
-synth (design testTree 200.0);;
+let timer = new System.Diagnostics.Stopwatch()
+timer.Start()
+generateTree (updateSize (design testTree) 100.0 0.0)
+printfn "Elapsed Time: %i" timer.ElapsedMilliseconds;;
