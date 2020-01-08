eb3161c748467649e1f55959046ca2abb04b5ad3
Antoine <antoine.sb@orange.fr>
2020-01-08 15:00:40 +0100

    :sparkles: add `fit_label`

--- Program.fs
+++ Program.fs
@@ -8,10 +8,18 @@ open AST
 open DrawingTree.ASTConverter
 open DrawingTree.Test
 
+let fit_label(label: string, font_size: int): string =
+    if 20 < label.Length then
+        (label.Substring 17) + "..."
+    else
+        label
+
 [<EntryPoint>]
 let main argv =
-
     let ast = P(test_declarations, [PrintLn(B(true))])
     let generic_tree = convert ast
+
+    let result = test_fast_concatenation
+    printfn "%f:%f" (fst result) (snd result)
 
     0
